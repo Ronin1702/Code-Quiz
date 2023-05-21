32928f5d39d5f752ae86ff5c574f68d8586fb9f9
Kai Chen <kai@ClaireHlsiPhone.localdomain>
2023-05-21 19:11:24 -0400

    I added a test question into questions.js

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,9 +1,9 @@
 // list of all questions, choices, and answers
 var questions = [
   {
-    title: 'Test Question',
-    choices: ['placeholder 1', 'placeholder 2', 'placeholder 3', 'placeholder 4'],
-    answer: 'placeholder 1',
+    title: 'Commonly used data types DO Not Include:',
+    choices: ['string', 'booleans', 'alerts', 'numbers'],
+    answer: 'numbers',
   },
 
 ];
