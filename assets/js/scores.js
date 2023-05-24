69f688f4b0d3db9cc108892448c685567faf25b2
Ronin1702 <kchen@me.com>
2023-05-24 18:26:58 -0400

    I updated some comments to better describe what I'm doing in each line of codes

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -2,13 +2,13 @@ function printHighscores() {
   // either get scores from localstorage or set to empty array
   var highscores = JSON.parse(window.localStorage.getItem('scoreslist')) || [];
 
-  // sort highscores by score property in descending order HINT: the sort method. 
+  // sort highscores by score property in descending order by using the sort method. 
   highscores.sort(function (a, b) {
     return b.score - a.score;
   })
 
   for (var i = 0; i < highscores.length; i += 1) {
-    // create li tag for each high score
+    // create li tag for each high score logged
     var liTag = document.createElement('li');
     liTag.textContent = highscores[i].initials + ' - ' + highscores[i].score;
 
